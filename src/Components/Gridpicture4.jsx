3b67d2f75fae947d27a30d8cf83c0bd610bea193
aman singh <amaninfo201415@gmail.com>
2024-03-03 23:55:21 +0530

    added

--- src/Components/Gridpicture4.jsx
+++ src/Components/Gridpicture4.jsx
@@ -39,7 +39,7 @@ export const Gridpicture4 = () => {
                     backgroundSize="cover" backgroundPosition="center">
                     <p className='font-bold p-5 text-2xl text-blue-1000'>Sweet treats Bring on </p>
                     <a className='p-5 underline'>shop now</a> </GridItem>
-                <GridItem className="cursor-pointer" borderRadius={15} rowSpan={1} colSpan={1} bgImage={`url(${image4})`}
+                <GridItem className="cursor-pointer" borderRadius={15} rowSpan={1} colSpan={1} bgImage={`url(${"https://i5.walmartimages.com/dfw/4ff9c6c9-ed3f/k2-_03e32269-d6ea-43f0-8a8d-e5d807f343fa.v1.jpg?odnHeight=342&odnWidth=248&odnBg=&odnDynImageQuality=70"})`}
                     backgroundSize="cover" backgroundPosition="center">
                     <p className='font-bold p-5 text-xl text-blue'>Fun & festive<br />decors</p>
                     <a className='hidden md:flex p-5 underline'>shop now</a></GridItem>
